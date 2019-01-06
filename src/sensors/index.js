4c8f32755495ed8db70b3269f75ae01ccbfc53f0
couturecraigj <couturecraigj@gmail.com>
2019-01-06 08:09:38 -0500

    removing vendor lookup

--- src/sensors/index.js
+++ src/sensors/index.js
@@ -3,13 +3,11 @@ const { platform } = require('os');
 const startSensing = () => {
   if (!['darwin', 'win32'].includes(platform())) {
     const find = require('local-devices');
-    const oui = require('oui');
 
     find().then(devices =>
       console.log(
-        devices
-          .filter(({ mac }) => mac !== '<incomplete>')
-          .map(v => ({ ...v, vendor: oui(v.mac) }))
+        devices.filter(({ mac }) => mac !== '<incomplete>')
+        // .map(v => ({ ...v, vendor: oui(v.mac) }))
       )
     );
     const temperature = require('./thermostat');
