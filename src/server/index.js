aed7d37a9528dc00f3f6f3db654646be18bd8a1a
couturecraigj <couturecraigj@gmail.com>
2019-01-07 11:01:34 -0500

    fix public directory serving

--- src/server/index.js
+++ src/server/index.js
@@ -9,7 +9,7 @@ const applyApollo = require('./graphQL').default;
 const app = express();
 
 app.use(cookieParser('SECRET'));
-app.use(express.static('public'));
+app.use(express.static(process.env.RAZZLE_PUBLIC_DIR));
 applyApollo(app);
 
 app.get('*', (req, res) => {
